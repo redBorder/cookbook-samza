bc6b1af26fc04d0261ad6a336397f0b6b5fcc360
Alberto Rodríguez <arodriguezdlc@gmail.com>
2016-11-16 18:36:39 +0100

    First working version

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -5,42 +5,134 @@
 
 action :add do #Usually used to install and configure something
   begin
-     # ... your code here ...
+    rb_samza_bi_dir = new_resource.rb_samza_bi_dir
+    config_dir = new_resource.config_dir
+    log_dir = new_resource.log_dir
+    rb_samza_bi_targz = "#{rb_samza_bi_dir}/app/rb-samza-bi.tar.gz"
+    num_containers = new_resource.num_containers
+    memory_per_container = new_resource.memory_per_container
+    indexing_topics = new_resource.indexing_topics
+    enrichment_topics = new_resource.enrichment_topics
+    autoscaling_topics = new_resource.autoscaling_topics
+    coordinator_replication_factor = new_resource.coordinator_replication_factor
+    checkpoint_replication_factor = new_resource.checkpoint_replication_factor
+    use_namespace = new_resource.use_namespace
 
-     template "PATH/template1" do
-       source "template1.erb"
-       cookbook "example"
-       #...
-     end
+    ##################################
+    # Samza package installation
+    ##################################
 
-     Chef::Log.info("Example cookbook has been processed")
+    yum_package "redborder-samza-bi" do
+      action :upgrade
+      flush_cache [:before]
+    end
+
+    ########################
+    # Directories creation
+    ########################
+    directory config_dir do
+      owner "root"
+      group "root"
+      mode "0755"
+    end
+
+    directory log_dir do
+      owner "hadoop"
+      group "hadoop"
+      mode "0755"
+    end
+
+    ##########################
+    # Retrieve databag data
+    ##########################
+    db_redborder = Chef::DataBagItem.load("passwords", "db_redborder") rescue db_redborder = {}
+    if !db_redborder.empty?
+      psql_uri = "#{db_redborder["hostname"]}:#{db_redborder["port"]}"
+      psql_user = db_redborder["username"]
+      psql_password = db_redborder["pass"]
+    end
+    kafka_topics = Chef::DataBagItem.load("backend", "kafka_topics") rescue kafka_topics = {}
+    if !kafka_topics.empty?
+      kafka_topics = kafka_topics["topics"]
+    end
+    ##################
+    # Templates
+    ##################
+
+    #INDEXING  
+    template "#{config_dir}/indexing.properties" do
+      source "rb-samza-bi_indexing.properties.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      cookbook "samza"
+      retries 2
+      variables(:rb_samza_bi_targz => rb_samza_bi_targz,
+                :num_containers => num_containers,
+                :memory_per_container => memory_per_container,
+                :indexing_topics => indexing_topics,
+                :autoscaling_topics => autoscaling_topics,
+                :coordinator_replication_factor => coordinator_replication_factor,
+                :checkpoint_replication_factor => checkpoint_replication_factor,
+                :kafka_topics => kafka_topics,
+                :use_namespace => use_namespace)
+    end
+    execute 'start_indexing_task' do
+      command "rb_samza.sh -e -t indexing"
+      timeout 60
+      ignore_failure true
+      action :run
+    end
+
+    #ENRICHMENT
+    template "#{config_dir}/enrichment.properties" do
+      source "rb-samza-bi_enrichment.properties.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      cookbook "samza"
+      retries 2
+      variables(:rb_samza_bi_targz => rb_samza_bi_targz,                
+                :num_containers => num_containers,
+                :memory_per_container => memory_per_container,
+                :enrichment_topics => enrichment_topics,
+                :coordinator_replication_factor => coordinator_replication_factor,
+                :checkpoint_replication_factor => checkpoint_replication_factor,
+                :use_namespace => use_namespace,
+                :psql_uri => psql_uri,
+                :psql_user => psql_user,
+                :psql_password => psql_password)
+    end    
+    execute 'start_enrichment_task' do
+      command "rb_samza.sh -e -t enrichment"
+      timeout 60
+      ignore_failure true
+      action :run
+    end
+
+    Chef::Log.info("Samza cookbook has been processed")
   rescue => e
     Chef::Log.error(e.message)
   end
 end
 
-action :remove do #Usually used to uninstall something
+action :remove do 
   begin
-     # ... your code here ...
-     Chef::Log.info("Example cookbook has been processed")
-  rescue => e
-    Chef::Log.error(e.message)
-  end
-end
+    config_dir = new_resource.config_dir
 
-action :register do #Usually used to register in consul
-  begin
-     # ... your code here ...
-     Chef::Log.info("Example cookbook has been processed")
-  rescue => e
-    Chef::Log.error(e.message)
-  end
-end
+    #####################
+    # Package uninstall
+    #####################
+    yum_package 'redborder-samza-bi' do
+      action :remove
+    end
 
-action :deregister do #Usually used to deregister from consul
-  begin
-     # ... your code here ...
-     Chef::Log.info("Example cookbook has been processed")
+    directory config_dir do
+      action :delete
+      recursive true
+    end
+
+    Chef::Log.info("Samza cookbook has been processed")
   rescue => e
     Chef::Log.error(e.message)
   end
--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -3,11 +3,19 @@
 # Resource:: config
 #
 
-actions :add, :remove, :register, :deregister
+actions :add, :remove
 default_action :add
 
-attribute :mystring, :kind_of => String, :default => "string example"
-attribute :myinteger, :kind_of => Fixnum, :default => 1
-attribute :myarray, :kind_of => Array, :default => ["val1"]
-attribute :myhash, :kind_of => Object, :default => {"val1" => "1"}
-attribute :myboolean, :kind_of => [TrueClass, FalseClass], :default => true
+attribute :rb_samza_bi_dir, :kind_of => String, :default => "/usr/lib/samza/rb-samza-bi"
+attribute :config_dir, :kind_of => String, :default => "/usr/lib/samza/rb-samza-bi/config"
+attribute :log_dir, :kind_of => String, :default => "/var/log/rb-samza-bi"
+attribute :num_containers, :kind_of => Integer, :default => 1
+attribute :memory_per_container, :kind_of => Integer, :default => 2560
+attribute :indexing_topics, :kind_of => Array, :default => [ "rb_flow_post", "rb_state_post",
+		"rb_loc_post", "rb_monitor", "rb_event_post", "rb_social", "rb_hashtag" ]
+attribute :enrichment_topics, :kind_of => Array, :default => [ "rb_flow", "rb_state", 
+		"rb_loc", "rb_event", "rb_nmsp", "rb_meraki", "rb_radius", "rb_pms" ]
+attribute :autoscaling_topics, :kind_of => Array, :default => [ "rb_flow_post", "rb_event_post" ]
+attribute :coordinator_replication_factor, :kind_of => Integer, :default => 1
+attribute :checkpoint_replication_factor, :kind_of => Integer, :default => 1
+attribute :use_namespace, :kind_of => [TrueClass, FalseClass] , :default => true
